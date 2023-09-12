181127b427fdc3d498c9b3922dea2c4c362abb7e
Mic Neale <michael.neale@gmail.com>
2023-09-12 17:26:19 -0400

    can discover PFIs

--- packages/http-client/src/client.ts
+++ packages/http-client/src/client.ts
@@ -99,6 +99,56 @@ export class TbdexHttpClient {
   }
 
   /**
+   * Discover PFIs that are anchored via did:ion. These have a type of "PFI" and an id of PFI.
+   * You can then query the endpoints for offerings.
+   */
+  static async discoverPFIs() {
+    const BASE_URL = 'https://ion.tbd.engineering'
+    const DID_TYPE_ENDPOINT = '/didtype/1669'
+    const IDENTIFIER_PREFIX = '/identifiers/'
+
+    async function fetchDIDList() {
+      const response = await fetch(BASE_URL + DID_TYPE_ENDPOINT)
+      if (!response.ok) {
+        throw new Error('Failed to fetch DID list')
+      }
+      return await response.json()
+    }
+
+    async function fetchDIDData(did) {
+      console.log(BASE_URL + IDENTIFIER_PREFIX + did)
+      const response = await fetch(BASE_URL + IDENTIFIER_PREFIX + did)
+      if (!response.ok) {
+        throw new Error('Failed to fetch DID data for ' + did)
+      }
+      return await response.json()
+    }
+
+    const ids = await fetchDIDList()
+    const promises = ids.map(id => {
+      const ionDid = 'did:ion:' + id
+      return fetchDIDData(ionDid)
+    })
+    const didDataList = await Promise.all(promises)
+
+    const pfiServiceEndpoints = didDataList.reduce((results, didData) => {
+      const services = didData.didDocument.service
+      const pfiServices = services.filter(service => service.type === 'PFI')
+
+      if (pfiServices.length > 0) {
+        results.push({
+          did             : didData.didDocument.id,
+          serviceEndpoint : pfiServices[0].serviceEndpoint
+        })
+      }
+
+      return results
+    }, [])
+
+    return pfiServiceEndpoints
+  }
+
+  /**
    * gets offerings from the pfi provided
    * @param _opts - options
    */
--- packages/http-client/tests/client.spec.ts
+++ packages/http-client/tests/client.spec.ts
@@ -1,7 +1,16 @@
 import { expect } from 'chai'
+import { TbdexHttpClient } from '../src/client.js'
 
 describe('client', () => {
   it('needs tests', () => {
     expect(true).to.be.true
+  })
+})
+
+describe('client', () => {
+  it('fetches PFIS', async () => {
+    const pfis = await TbdexHttpClient.discoverPFIs()
+    console.log(pfis)
+    expect(pfis).to.be.an('array')
   })
 })
