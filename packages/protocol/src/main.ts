4012ae78fb92bf06b46f14ee8326f93e58bc1ecc
Leo Ribeiro <leonardo@squareup.com>
2023-10-03 19:33:11 -0400

    fix changes

--- packages/protocol/src/main.ts
+++ packages/protocol/src/main.ts
@@ -8,17 +8,17 @@
  * @packageDocumentation
  */
 
-import { messageFactory, resourceFactory } from "./utils.js";
-import { Resource } from "./resource.js";
-import { Message } from "./message.js";
+import { messageFactory, resourceFactory } from './utils.js'
+import { Resource } from './resource.js'
+import { Message } from './message.js'
 
-Message.factory = messageFactory;
-Resource.factory = resourceFactory;
+Message.factory = messageFactory
+Resource.factory = resourceFactory
 
-export * from "./resource-kinds/index.js";
-export * from "./message-kinds/index.js";
-export * from "./did-resolver.js";
-export * from "./dev-tools.js";
-export * from "./crypto.js";
-export * from "./types.js";
-export { Message, Resource };
+export * from './resource-kinds/index.js'
+export * from './message-kinds/index.js'
+export * from './did-resolver.js'
+export * from './dev-tools.js'
+export * from './crypto.js'
+export * from './types.js'
+export { Message, Resource }
