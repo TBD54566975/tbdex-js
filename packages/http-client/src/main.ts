95cf0c71141db8e4003ba4a0b2667061c9e3ab2f
Leo Ribeiro <leordev@gmail.com>
2023-10-12 12:56:54 -0400

    Add packageDocs

--- packages/http-client/src/main.ts
+++ packages/http-client/src/main.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * A client that can be used to interface with tbDEX PFIs over an HTTP transport
  *
@@ -6,6 +7,17 @@
  * @packageDocumentation
  */
 
+||||||| parent of 17c8ecd (Add packageDocs)
+=======
+/**
+ * An HTTP client that can be used to send tbdex messages to PFIs
+ *
+ * [Link to GitHub Repo](https://github.com/TBD54566975/tbdex-js/tree/main/packages/http-client)
+ *
+ * @packageDocumentation
+ */
+
+>>>>>>> 17c8ecd (Add packageDocs)
 export * from '@tbdex/protocol'
 export * from './client.js'
 export * from './types.js'
