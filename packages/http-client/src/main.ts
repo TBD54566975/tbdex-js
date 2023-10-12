17c8ecd5cc2a25b3abf4c27018c19788024a723d
Leo Ribeiro <leordev@gmail.com>
2023-10-12 12:56:54 -0400

    Add packageDocs

--- packages/http-client/src/main.ts
+++ packages/http-client/src/main.ts
@@ -1,3 +1,11 @@
+/**
+ * An HTTP client that can be used to send tbdex messages to PFIs
+ *
+ * [Link to GitHub Repo](https://github.com/TBD54566975/tbdex-js/tree/main/packages/http-client)
+ *
+ * @packageDocumentation
+ */
+
 export * from '@tbdex/protocol'
 export * from './client.js'
 export * from './types.js'
--- packages/http-server/src/main.ts
+++ packages/http-server/src/main.ts
@@ -1,3 +1,11 @@
+/**
+ * A configurable implementation of the [tbdex http api draft specification](https://github.com/TBD54566975/tbdex-protocol/blob/main/rest-api/README.md)
+ *
+ * [Link to GitHub Repo](https://github.com/TBD54566975/tbdex-js/tree/main/packages/http-server)
+ *
+ * @packageDocumentation
+ */
+
 export * from '@tbdex/http-client'
 export * from './http-server.js'
 export * from './types.js'
