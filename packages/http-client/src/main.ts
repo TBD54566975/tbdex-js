112be05e5d71d8310ec1132a7bb794bc73b02081
Kendall Weihe <kendallw@squareup.com>
2023-10-17 07:51:54 -0400

    Fixed git conflict in http-client's main

--- packages/http-client/src/main.ts
+++ packages/http-client/src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * A client that can be used to interface with tbDEX PFIs over an HTTP transport
  *
@@ -7,17 +6,6 @@
  * @packageDocumentation
  */
 
-||||||| parent of 17c8ecd (Add packageDocs)
-=======
-/**
- * An HTTP client that can be used to send tbdex messages to PFIs
- *
- * [Link to GitHub Repo](https://github.com/TBD54566975/tbdex-js/tree/main/packages/http-client)
- *
- * @packageDocumentation
- */
-
->>>>>>> 17c8ecd (Add packageDocs)
 export * from '@tbdex/protocol'
 export * from './client.js'
 export * from './types.js'
