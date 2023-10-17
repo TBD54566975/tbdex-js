27ba4470878db6ee0bbb11b69ceb3cbabb87f3a2
Leo Ribeiro <leordev@gmail.com>
2023-10-17 07:43:49 -0400

    Add http client and server docs (#42)
    * Add http client and server docs
    
    * set declarationMap = true for http-server
    
    * disable test-with-browsers
    
    * Add http client and server to tbdocs publish
    
    * update tbdocs action version
    
    * Add packageDocs
    
    * Adjusted doc language in http-client enytry point
    
    ---------
    
    Co-authored-by: Kendall Weihe <kendallw@squareup.com>

--- packages/http-client/src/main.ts
+++ packages/http-client/src/main.ts
@@ -1,3 +1,11 @@
+/**
+ * A client that can be used to interface with tbDEX PFIs over an HTTP transport
+ *
+ * [Link to GitHub Repo](https://github.com/TBD54566975/tbdex-js/tree/main/packages/http-client)
+ *
+ * @packageDocumentation
+ */
+
 export * from '@tbdex/protocol'
 export * from './client.js'
 export * from './types.js'
