9f60e48700c6d22fbd40219229f6cc0d82d29f13
Moe Jangda <moe@tbd.email>
2023-10-22 16:05:51 -0700

    remove test vectors scaffolding

--- packages/protocol/tests/vectors.spec.ts
+++
@@ -1,8 +0,0 @@
-import testVectors from './vectors.json' assert { type: 'json' }
-import { Offering } from '../src/main.js'
-
-describe('Vectors', () => {
-  xit('works', async () => {
-    const offering = await Offering.parse(testVectors.resources.offering as any)
-  })
-})
