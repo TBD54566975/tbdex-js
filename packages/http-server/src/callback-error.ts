e31fa7ba252e67574897fab180246023c6920a08
Leo Ribeiro <leordev@gmail.com>
2023-12-18 21:25:09 -0500

    Generates the HTML to publish to GH Pages (#116)
    * Generates the HTML to publish to GH Pages
    
    * fix commenter
    
    * Publish Docs to GH Pages
    
    * test Publish Docs to GH Pages
    
    * fix issues

--- packages/http-server/src/callback-error.ts
+++ packages/http-server/src/callback-error.ts
@@ -6,7 +6,10 @@ import type { ErrorDetail } from '@tbdex/http-client'
  * @beta
  */
 export class CallbackError extends Error {
+  /** Error HTTP Response status code: 400 - Bad Request, 401 - Unauthorized, 500 etc.  */
   public readonly statusCode: number
+
+  /** List with full error details objects received from the PFI server response */
   public readonly details: ErrorDetail[]
 
   constructor(statusCode: number, details: ErrorDetail[] = []) {
