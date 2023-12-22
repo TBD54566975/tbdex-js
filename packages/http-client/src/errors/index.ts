470058e27f437af9ec8815d4f84c873cd2b902a3
Moe Jangda <moe@tbd.email>
2023-12-22 01:20:19 -0600

    finish refactoring `generateRequestToken` and `verifyRequestToken`

--- packages/http-client/src/errors/index.ts
+++ packages/http-client/src/errors/index.ts
@@ -1,3 +1,4 @@
 export { RequestError } from './request-error.js'
 export { ResponseError } from './response-error.js'
+export { RequestTokenError } from './request-token-error.js'
 export { ValidationError, InvalidDidError, MissingServiceEndpointError } from './validation-error.js'
