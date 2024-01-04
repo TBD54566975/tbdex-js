38aca4db0ee3cccf1309ae1231b98c2645fdc49b
Diane Huxley <dhuxley@tbd.email>
2024-01-04 11:43:57 -0800

    Add tests using all existing test vectors

--- packages/protocol/tests/test-vectors.spec.ts
+++ packages/protocol/tests/test-vectors.spec.ts
@@ -1,7 +1,8 @@
 
 import chai, { expect } from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import { Rfq } from '../src/message-kinds/rfq.js'
+import { Close, Order, OrderStatus, Quote, Rfq } from '../src/message-kinds/index.js'
+import { Offering } from '../src/resource-kinds/index.js'
 import { loadOrFetchTestVector } from './fetch-test-vectors.js'
 
 chai.use(chaiAsPromised)
@@ -11,5 +12,35 @@ describe('Test vectors: parse and serialize', () => {
     const testVector = await loadOrFetchTestVector('parse-rfq.json')
     const rfq = await Rfq.parse(testVector.input)
     expect(rfq.toJSON()).to.deep.eq(testVector.output)
+  })
+
+  it('parse-order.json', async () => {
+    const testVector = await loadOrFetchTestVector('parse-order.json')
+    const order = await Order.parse(testVector.input)
+    expect(order.toJSON()).to.deep.eq(testVector.output)
+  })
+
+  it('parse-orderstatus.json', async () => {
+    const testVector = await loadOrFetchTestVector('parse-orderstatus.json')
+    const orderstatus = await OrderStatus.parse(testVector.input)
+    expect(orderstatus.toJSON()).to.deep.eq(testVector.output)
+  })
+
+  it('parse-quote.json', async () => {
+    const testVector = await loadOrFetchTestVector('parse-quote.json')
+    const quote = await Quote.parse(testVector.input)
+    expect(quote.toJSON()).to.deep.eq(testVector.output)
+  })
+
+  it('parse-close.json', async () => {
+    const testVector = await loadOrFetchTestVector('parse-close.json')
+    const close = await Close.parse(testVector.input)
+    expect(close.toJSON()).to.deep.eq(testVector.output)
+  })
+
+  it('parse-offering.json', async() => {
+    const testVector = await loadOrFetchTestVector('parse-offering.json')
+    const offering = await Offering.parse(testVector.input)
+    expect(offering.toJSON()).to.deep.eq(testVector.output)
   })
 })
