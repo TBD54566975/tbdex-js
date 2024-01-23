d7b445452a718d2adc6e6d7e59866af6ffb60e8b
Jiyoon Koo <jiyoon@squareup.com>
2024-01-23 11:17:07 -0500

    reverting changes to rfq spec

--- packages/protocol/tests/rfq.spec.ts
+++ packages/protocol/tests/rfq.spec.ts
@@ -1,39 +1,17 @@
-import type { CreateRfqOptions, OfferingData, RfqData } from '../src/main.js'
-
 import { VerifiableCredential } from '@web5/credentials'
-import { DidKeyMethod } from '@web5/dids'
-import { Rfq, Offering } from '../src/main.js'
+import type { CreateRfqOptions, Offering } from '../src/main.js'
+
+import { Rfq, DevTools } from '../src/main.js'
 import { Convert } from '@web5/common'
 import { expect } from 'chai'
-
-const rfqData: RfqData = {
-  offeringId  : 'abcd123',
-  payinMethod : {
-    kind           : 'DEBIT_CARD',
-    paymentDetails : {
-      'cardNumber'     : '1234567890123456',
-      'expiryDate'     : '12/22',
-      'cardHolderName' : 'Ephraim Bartholomew Winthrop',
-      'cvv'            : '123'
-    }
-  },
-  payoutMethod: {
-    kind           : 'BTC_ADDRESS',
-    paymentDetails : {
-      btcAddress: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'
-    }
-  },
-  payinAmount : '20000.00',
-  claims      : ['']
-}
 
 describe('Rfq', () => {
   describe('create', () => {
     it('creates an rfq', async () => {
-      const alice = await DidKeyMethod.create()
+      const alice = await DevTools.createDid()
       const message = Rfq.create({
         metadata : { from: alice.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       expect(message.id).to.exist
@@ -68,10 +46,10 @@ describe('Rfq', () => {
 
   describe('sign', () => {
     it('sets signature property', async () => {
-      const did = await DidKeyMethod.create()
+      const did = await DevTools.createDid()
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       await rfq.sign(did)
@@ -81,28 +59,28 @@ describe('Rfq', () => {
     })
 
     it('includes alg and kid in jws header', async () => {
-      const did = await DidKeyMethod.create()
+      const did = await DevTools.createDid()
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       await rfq.sign(did)
 
-      const [base64UrlEncodedJwsHeader] = rfq.signature.split('.')
-      const jwsHeader = Convert.base64Url(base64UrlEncodedJwsHeader).toObject()
+      const [base64UrlEncodedJwsHeader] = rfq.signature!.split('.')
+      const jwsHeader: { kid?: string, alg?: string}  = Convert.base64Url(base64UrlEncodedJwsHeader).toObject()
 
-      expect(jwsHeader['kid']).to.equal(did.document.verificationMethod[0].id)
+      expect(jwsHeader['kid']).to.equal(did.document.verificationMethod![0].id)
       expect(jwsHeader['alg']).to.exist
     })
   })
 
   describe('verify', () => {
     it('does not throw an exception if message integrity is intact', async () => {
-      const did = await DidKeyMethod.create()
+      const did = await DevTools.createDid()
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       await rfq.sign(did)
@@ -110,10 +88,10 @@ describe('Rfq', () => {
     })
 
     it('throws an error if no signature is present on the message provided', async () => {
-      const alice = await DidKeyMethod.create()
+      const alice = await DevTools.createDid()
       const rfq = Rfq.create({
         metadata : { from: alice.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       try {
@@ -144,10 +122,10 @@ describe('Rfq', () => {
     })
 
     it('returns an instance of Message if parsing is successful', async () => {
-      const did = await DidKeyMethod.create()
+      const did = await DevTools.createDid()
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : rfqData
+        data     : await DevTools.createRfqData()
       })
 
       await rfq.sign(did)
@@ -159,21 +137,13 @@ describe('Rfq', () => {
     })
   })
 
-  describe('verifyOfferingRequirements', () => {
-    const offering: Offering = createUnsignedOffering()
-    const rfqOptions: CreateRfqOptions = {
-      metadata: {
-        from : '',
-        to   : 'did:ex:pfi'
-      },
-      data: {
-        ...rfqData,
-        offeringId: offering.id,
-      }
-    }
-    before(async () => {
-      const did = await DidKeyMethod.create()
-      const vc = await VerifiableCredential.create({
+  describe('verifyOfferingRequirements', async () => {
+    let offering: Offering
+    let rfqOptions: CreateRfqOptions
+
+    beforeEach(async () => {
+      const did = await DevTools.createDid()
+      const vc = await VerifiableCredential.create({ // this credential fulfills the offering's required claims
         type    : 'SanctionsCredential',
         issuer  : did.did,
         subject : did.did,
@@ -182,8 +152,19 @@ describe('Rfq', () => {
         }
       })
 
+      offering = DevTools.createOffering()
       const vcJwt = await vc.sign({ did })
 
+      rfqOptions = {
+        metadata: {
+          from : '',
+          to   : 'did:ex:pfi'
+        },
+        data: {
+          ...await DevTools.createRfqData(),
+          offeringId: offering.id,
+        }
+      }
       rfqOptions.metadata.from = did.did
       rfqOptions.data.claims = [vcJwt]
     })
@@ -205,7 +186,6 @@ describe('Rfq', () => {
     })
 
     it('throws an error if payinAmount exceeds the provided offering\'s maxAmount', async () => {
-      const offering = createUnsignedOffering()
       offering.payinCurrency.maxAmount = '0.01'
 
       const rfq = Rfq.create({
@@ -307,10 +287,9 @@ describe('Rfq', () => {
 
   describe('verifyClaims', () => {
     it(`does not throw an exception if an rfq's claims fulfill the provided offering's requirements`, async () => {
-      const did = await DidKeyMethod.create()
-      const offering = createUnsignedOffering()
-
-      const vc = await VerifiableCredential.create({
+      const did = await DevTools.createDid()
+      const offering = DevTools.createOffering()
+      const vc = await VerifiableCredential.create({ // this credential fulfills the offering's required claims
         type    : 'SanctionsCredential',
         issuer  : did.did,
         subject : did.did,
@@ -321,37 +300,20 @@ describe('Rfq', () => {
 
       const vcJwt = await vc.sign({ did })
 
+      const rfqData = await DevTools.createRfqData()
+      rfqData.claims = [vcJwt]
+
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : {
-          offeringId  : 'abcd123',
-          payinMethod : {
-            kind           : 'DEBIT_CARD',
-            paymentDetails : {
-              'cardNumber'     : '1234567890123456',
-              'expiryDate'     : '12/22',
-              'cardHolderName' : 'Ephraim Bartholomew Winthrop',
-              'cvv'            : '123'
-            }
-          },
-          payoutMethod: {
-            kind           : 'BTC_ADDRESS',
-            paymentDetails : {
-              btcAddress: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'
-            }
-          },
-          payinAmount : '20000.00',
-          claims      : [vcJwt]
-        }
+        data     : rfqData
       })
 
       await rfq.verifyClaims(offering)
     })
 
     it(`throws an exception if an rfq's claims dont fulfill the provided offering's requirements`, async () => {
-      const did = await DidKeyMethod.create()
-      const offering = createUnsignedOffering()
-
+      const did = await DevTools.createDid()
+      const offering = DevTools.createOffering()
       const vc = await VerifiableCredential.create({
         type    : 'PuupuuCredential',
         issuer  : did.did,
@@ -361,30 +323,14 @@ describe('Rfq', () => {
         }
       })
 
-      const vcJwt = await vc.sign({ did })
+      const vcJwt = await vc.sign({ did})
+
+      const rfqData = await DevTools.createRfqData()
+      rfqData.claims = [vcJwt]
 
       const rfq = Rfq.create({
         metadata : { from: did.did, to: 'did:ex:pfi' },
-        data     : {
-          offeringId  : 'abcd123',
-          payinMethod : {
-            kind           : 'DEBIT_CARD',
-            paymentDetails : {
-              'cardNumber'     : '1234567890123456',
-              'expiryDate'     : '12/22',
-              'cardHolderName' : 'Ephraim Bartholomew Winthrop',
-              'cvv'            : '123'
-            }
-          },
-          payoutMethod: {
-            kind           : 'BTC_ADDRESS',
-            paymentDetails : {
-              btcAddress: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa'
-            }
-          },
-          payinAmount : '20000.00',
-          claims      : [vcJwt]
-        }
+        data     : rfqData
       })
 
       try {
@@ -395,96 +341,3 @@ describe('Rfq', () => {
     })
   })
 })
-
-function createUnsignedOffering() {
-  const offeringData: OfferingData = {
-    description   : 'Selling BTC for USD',
-    payinCurrency : {
-      currencyCode : 'USD',
-      maxAmount    : '99999999.00'
-    },
-    payoutCurrency: {
-      currencyCode : 'BTC',
-      maxAmount    : '99952611.00000'
-    },
-    payoutUnitsPerPayinUnit : '0.00003826',
-    payinMethods            : [{
-      kind                   : 'DEBIT_CARD',
-      requiredPaymentDetails : {
-        $schema    : 'http://json-schema.org/draft-07/schema',
-        type       : 'object',
-        properties : {
-          cardNumber: {
-            type        : 'string',
-            description : 'The 16-digit debit card number',
-            minLength   : 16,
-            maxLength   : 16
-          },
-          expiryDate: {
-            type        : 'string',
-            description : 'The expiry date of the card in MM/YY format',
-            pattern     : '^(0[1-9]|1[0-2])\\/([0-9]{2})$'
-          },
-          cardHolderName: {
-            type        : 'string',
-            description : 'Name of the cardholder as it appears on the card'
-          },
-          cvv: {
-            type        : 'string',
-            description : 'The 3-digit CVV code',
-            minLength   : 3,
-            maxLength   : 3
-          }
-        },
-        required             : ['cardNumber', 'expiryDate', 'cardHolderName', 'cvv'],
-        additionalProperties : false
-      }
-    }],
-    payoutMethods: [{
-      kind                   : 'BTC_ADDRESS',
-      requiredPaymentDetails : {
-        $schema    : 'http://json-schema.org/draft-07/schema',
-        type       : 'object',
-        properties : {
-          btcAddress: {
-            type        : 'string',
-            description : 'your Bitcoin wallet address'
-          }
-        },
-        required             : ['btcAddress'],
-        additionalProperties : false
-      }
-    }],
-    requiredClaims: {
-      id     : '7ce4004c-3c38-4853-968b-e411bafcd945',
-      format : {
-        'jwt_vc': {
-          'alg': [
-            'ES256K',
-            'EdDSA'
-          ]
-        }
-      },
-      input_descriptors: [{
-        id          : 'bbdb9b7c-5754-4f46-b63b-590bada959e0',
-        constraints : {
-          fields: [{
-            path: [
-              '$.vc.type[*]',
-              '$.type[*]'
-            ],
-            filter: {
-              type    : 'string',
-              pattern : '^SanctionsCredential$'
-            }
-          }]
-        }
-      }]
-    }
-  }
-
-  return Offering.create({
-    metadata : { from: 'did:ex:pfi' },
-    data     : offeringData
-  })
-}
