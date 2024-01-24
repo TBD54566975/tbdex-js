c7352aa663569507400f1a74502a7289e8ffb7af
Kirah Sapong <kirah@tbd.email>
2024-01-23 17:00:04 -0800

    Add glob to build for protocol tests

--- packages/protocol/tests/buffer-polyfill.cjs
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const Buffer = require('buffer/').Buffer
-
-globalThis.Buffer = Buffer
--- packages/protocol/tests/bundle-tests.mjs
+++ packages/protocol/tests/bundle-tests.mjs
@@ -2,25 +2,21 @@ import { fileURLToPath } from 'node:url'
 
 import esbuild from 'esbuild'
 import path from 'node:path'
+import glob from 'tiny-glob'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 esbuild.buildSync({
-  entryPoints: [
-    `${__dirname}/close.spec.ts`,
-    `${__dirname}/crypto.spec.ts`,
-    `${__dirname}/offering.spec.ts`,
-    `${__dirname}/rfq.spec.ts`
-  ],
-  format    : 'esm',
-  bundle    : true,
-  sourcemap : true,
-  platform  : 'browser',
-  target    : ['chrome101', 'firefox108', 'safari16'],
-  outdir    : `${__dirname}/compiled`,
-  define    : {
+  //TODO: fix test vectors spec
+  entryPoints : await glob(`${__dirname}/!(test-vectors)*.spec.ts`),
+  format      : 'esm',
+  bundle      : true,
+  sourcemap   : true,
+  platform    : 'browser',
+  target      : ['chrome101', 'firefox108', 'safari16'],
+  outdir      : `${__dirname}/compiled`,
+  define      : {
     'global': 'globalThis',
-  },
-  // inject: [`${__dirname}/buffer-polyfill.cjs`],
+  }
 })
