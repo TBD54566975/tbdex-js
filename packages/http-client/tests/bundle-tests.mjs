e34adf01a45b632b4191583dc9e39ab8178bd9b9
Kirah Sapong <kirah@tbd.email>
2024-01-23 16:01:08 -0800

    Add glob to build for  http-client browser tests

--- packages/http-client/tests/bundle-tests.mjs
+++ packages/http-client/tests/bundle-tests.mjs
@@ -2,12 +2,13 @@ import { fileURLToPath } from 'node:url'
 
 import esbuild from 'esbuild'
 import path from 'node:path'
+import glob from 'tiny-glob'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 esbuild.buildSync({
-  entryPoints : [`${__dirname}/client.spec.ts`],
+  entryPoints : await glob(`${__dirname}/*.spec.ts`),
   format      : 'esm',
   bundle      : true,
   sourcemap   : true,
